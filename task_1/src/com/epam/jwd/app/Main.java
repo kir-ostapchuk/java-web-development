1a20b10f57e072b617d9d19277150f25fa7cfee1
xfase <xfaseex1@gmail.com>
2020-11-24 10:53:07 +0300

    refactor: change figure creation

--- task_1/src/com/epam/jwd/app/Main.java
+++ task_1/src/com/epam/jwd/app/Main.java
@@ -12,50 +12,56 @@ public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-// 1.
-        Point[] points = new Point[4];
-        for (int i = 0; i < points.length; i++) {
+
+        Point[] points = createPoints(4);
+        Line[] lines = createLines(2);
+        Triangle[] triangles = createTriangles(2);
+        Square[] squares = createSquares(1);
+
+    }
+
+    private static Point[] createPoints(int length) {
+        Point[] points = new Point[length];
+        for (int i = 0; i < length; i++) {
             points[i] = new Point(i, i + 7);
         }
-// 2.
-        Line[] lines = new Line[2];
-        for (int i = 0; i < lines.length; i++) {
+
+        return points;
+    }
+
+    private static Line[] createLines(int length) {
+        Line[] lines = new Line[length];
+        for (int i = 0; i < length; i++) {
             lines[i] = new Line(
                     new Point(i, i + 7),
                     new Point(i + 2, i + 9));
         }
-// 3.
-        Triangle[] triangles = new Triangle[2];
-        for (int i = 0; i < triangles.length; i++) {
+
+        return lines;
+    }
+
+    private static Triangle[] createTriangles(int length) {
+        Triangle[] triangles = new Triangle[length];
+        for (int i = 0; i < length; i++) {
             triangles[i] = new Triangle(
                     new Point(i, i + 7),
                     new Point(i + 1, i + 12),
                     new Point(i + 2, i + 9));
         }
-// 4.
-        Square[] squares = new Square[1];
-        for (int i = 0; i < squares.length; i++) {
+
+        return triangles;
+    }
+
+    private static Square[] createSquares(int length) {
+        Square[] squares = new Square[length];
+        for (int i = 0; i < length; i++) {
             squares[i] = new Square(
                     new Point(i + 7, i),
                     new Point(i, i),
                     new Point(i, i + 3),
                     new Point(i + 7, i + 3));
         }
-// 5.
 
-// 6.
-// 6.1.
-        for (Line line : lines) {
-            LOGGER.info(line.toString());
-        }
-
-        if(triangles.length > 0) {
-            for (Triangle triangle : triangles) {
-                LOGGER.info(triangle.toString());
-            }
-        }
-        else {
-            //LOGGER.error(triangle);
-        }
+        return squares;
     }
 }
