a70343d71e50c54bad8a661204a275cd3f50b2a0
xfase <xfaseex1@gmail.com>
2020-11-27 03:34:48 +0300

    refactor: delete unnecessary methods in Square

--- task_1/src/com/epam/jwd/model/Square.java
+++ task_1/src/com/epam/jwd/model/Square.java
@@ -21,47 +21,31 @@ public class Square {
         return point1;
     }
 
-    public void setPoint1(Point point1) {
-        this.point1 = point1;
-    }
+//    public void setPoint1(Point point1) {
+//        this.point1 = point1;
+//    }
 
     public Point getPoint2() {
         return point2;
     }
 
-    public void setPoint2(Point point2) {
-        this.point2 = point2;
-    }
+//    public void setPoint2(Point point2) {
+//        this.point2 = point2;
+//    }
 
     public Point getPoint3() {
         return point3;
     }
 
-    public void setPoint3(Point point3) {
-        this.point3 = point3;
-    }
+//    public void setPoint3(Point point3) {
+//        this.point3 = point3;
+//    }
 
     public Point getPoint4() {
         return point4;
     }
 
-    public void setPoint4(Point point4) {
-        this.point4 = point4;
-    }
-
-    private double calculateSide(Point point1, Point point2) {
-        return Math.hypot(
-                point1.getX() - point2.getX(),
-                point1.getY() - point2.getY());
-    }
-
-    @Override
-    public String toString() {
-        return "Square{" +
-                "point1=" + point1 +
-                ", point2=" + point2 +
-                ", point3=" + point3 +
-                ", point4=" + point4 +
-                '}';
-    }
+//    public void setPoint4(Point point4) {
+//        this.point4 = point4;
+//    }
 }
