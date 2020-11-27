3ffd98ab0eedb89dd374959b2a3cd0f11b98f5b2
xfase <xfaseex1@gmail.com>
2020-11-27 03:36:25 +0300

    refactor: delete unnnecessary methods in Triangle

--- task_1/src/com/epam/jwd/model/Triangle.java
+++ task_1/src/com/epam/jwd/model/Triangle.java
@@ -16,32 +16,23 @@ public class Triangle {
         return point1;
     }
 
-    public void setPoint1(Point point1) {
-        this.point1 = point1;
-    }
+//    public void setPoint1(Point point1) {
+//        this.point1 = point1;
+//    }
 
     public Point getPoint2() {
         return point2;
     }
 
-    public void setPoint2(Point point2) {
-        this.point2 = point2;
-    }
+//    public void setPoint2(Point point2) {
+//        this.point2 = point2;
+//    }
 
     public Point getPoint3() {
         return point3;
     }
 
-    public void setPoint3(Point point3) {
-        this.point3 = point3;
-    }
-
-    @Override
-    public String toString() {
-        return "Triangle{" +
-                "point1=" + point1 +
-                ", point2=" + point2 +
-                ", point3=" + point3 +
-                '}';
-    }
+//    public void setPoint3(Point point3) {
+//        this.point3 = point3;
+//    }
 }
