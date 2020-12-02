0d27e6930fb3ca1c5ff7ad31a83819796a7097c5
xfase <xfaseex1@gmail.com>
2020-12-02 22:10:47 +0300

    refactor: change final vars names

--- task_1/src/com/epam/jwd/app/Main.java
+++ task_1/src/com/epam/jwd/app/Main.java
@@ -7,7 +7,6 @@ import com.epam.jwd.model.triangle.Triangle;
 import com.epam.jwd.model.square.Square;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,28 +14,28 @@ public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
-    private static final List<Integer> xPointCoordinates = Arrays.asList(1, 2, 3, 4);
-    private static final List<Integer> yPointCoordinates = Arrays.asList(4, 3, 2, 1);
+    private static final List<Integer> X_POINT_COORDINATES = Arrays.asList(1, 2, 3, 4);
+    private static final List<Integer> Y_POINT_COORDINATES = Arrays.asList(4, 3, 2, 1);
 
-    private static final List<Integer> xLineCoordinates = Arrays.asList(-1, -2, -3, -4);
-    private static final List<Integer> yLineCoordinates = Arrays.asList(-4, -3, -2, -1);
+    private static final List<Integer> X_LINE_COORDINATES = Arrays.asList(-1, -2, -3, -4);
+    private static final List<Integer> Y_LINE_COORDINATES = Arrays.asList(-4, -3, -2, -1);
 
-    private static final List<Integer> xTriangleCoordinates = Arrays.asList(-4, 3, -2, -1, 8, 9);
-    private static final List<Integer> yTriangleCoordinates = Arrays.asList(-4, -3, 2, -1, -8, -9);
+    private static final List<Integer> X_TRIANGLE_COORDINATES = Arrays.asList(-4, 3, -2, -1, 8, 9);
+    private static final List<Integer> Y_TRIANGLE_COORDINATES = Arrays.asList(-4, -3, 2, -1, -8, -9);
 
-    private static final List<Integer> xSquareCoordinates = Arrays.asList(1, 0, 1, 0);
-    private static final List<Integer> ySquareCoordinates = Arrays.asList(0, 1, 1, 0);
+    private static final List<Integer> X_SQUARE_COORDINATES = Arrays.asList(1, 0, 1, 0);
+    private static final List<Integer> Y_SQUARE_COORDINATES = Arrays.asList(0, 1, 1, 0);
 
-    private static final List<Integer> xPentagonCoordinates = Arrays.asList(1, 2, 3, 4, 5);
-    private static final List<Integer> yPentagonCoordinates = Arrays.asList(4, 3, 2, 1, 6);
+    private static final List<Integer> X_PENTAGON_COORDINATES = Arrays.asList(1, 2, 3, 4, 5);
+    private static final List<Integer> Y_PENTAGON_COORDINATES = Arrays.asList(4, 3, 2, 1, 6);
 
     public static void main(String[] args) {
         try {
-            List<Point> points = MainUtility.createPoints(xPointCoordinates, yPointCoordinates);
-            List<Line> lines = MainUtility.createLines(xLineCoordinates, yLineCoordinates);
-            List<Triangle> triangles = MainUtility.createTriangles(xTriangleCoordinates, yTriangleCoordinates);
-            List<Square> squares = MainUtility.createSquares(xSquareCoordinates, ySquareCoordinates);
-            List<Pentagon> pentagons = MainUtility.createPentagons(xPentagonCoordinates, yPentagonCoordinates);
+            List<Point> points = MainUtility.createPoints(X_POINT_COORDINATES, Y_POINT_COORDINATES);
+            List<Line> lines = MainUtility.createLines(X_LINE_COORDINATES, Y_LINE_COORDINATES);
+            List<Triangle> triangles = MainUtility.createTriangles(X_TRIANGLE_COORDINATES, Y_TRIANGLE_COORDINATES);
+            List<Square> squares = MainUtility.createSquares(X_SQUARE_COORDINATES, Y_SQUARE_COORDINATES);
+            List<Pentagon> pentagons = MainUtility.createPentagons(X_PENTAGON_COORDINATES, Y_PENTAGON_COORDINATES);
         } catch (IllegalArgumentException exception) {
             LOGGER.error(exception.getMessage());
         }
